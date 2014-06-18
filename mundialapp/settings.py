2f48ef7bf2730675d5966b77de2204d56628b6b6
enzoruiz <enzo.rp.90@gmail.com>
2014-06-18 18:47:24 -0500

    primer commit

--- mundialapp/settings.py
+++ mundialapp/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2] + ['static'])
+STATIC_ROOT = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2] + ['staticfiles'])
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
